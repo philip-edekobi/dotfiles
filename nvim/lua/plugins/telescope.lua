f7be10985de44e0d09127136175165f9cea6cbc2
Philip Edekobi <philipedekobi@gmail.com>
2026-01-04 09:34:49 +0100

    fix weird intermittent message during telescope search

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
+		-- branch = "0.1.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
@@ -13,6 +13,9 @@ return {
 			telescope.setup({
 				defaults = {
 					path_display = { "smart" },
+					preview = {
+						treesitter = false,
+					},
 					mappings = {
 						i = {
 							["<C-k>"] = require("telescope.actions").move_selection_previous,
